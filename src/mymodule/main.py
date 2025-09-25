b5295018e236d88dc1ca80f21fa4121d85b5a057
Pierre Formont <pierre@olyzon.tv>
2025-09-25 16:50:30 +0200

    add more imports

--- src/mymodule/main.py
+++ src/mymodule/main.py
@@ -1,2 +1,22 @@
+from pydantic import BaseModel
+from pyiceberg.catalog import load_catalog
+
+
 def add(a, b):
     return a + b
+
+
+class AddRequest(BaseModel):
+    a: int
+    b: int
+
+
+warehouse_path = "/tmp/warehouse"
+catalog = load_catalog(
+    "default",
+    **{
+        "type": "sql",
+        "uri": f"sqlite:///{warehouse_path}/pyiceberg_catalog.db",
+        "warehouse": f"file://{warehouse_path}",
+    },
+)
